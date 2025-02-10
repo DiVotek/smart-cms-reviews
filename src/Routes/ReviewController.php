650108c23115ac7d6ebf276c670251789328a29e
maxboyko <maxboiko@divotek.com>
2025-02-10 15:55:57 +0200

    fix for reviews

--- src/Routes/ReviewController.php
+++ src/Routes/ReviewController.php
@@ -16,30 +16,31 @@ class ReviewController
             'product_id' => 'required|exists:SmartCms\Store\Models\Product,id',
             'rating' => 'required|integer|min:1|max:5',
         ];
-        $fastOrderFields = setting('reviews.fields', []);
-        $customAttributes = [];
-        foreach ($fastOrderFields as $field) {
-            $field = Field::query()->find($field['field_id']);
-            if ($field->required) {
-                $validation[strtolower($field->html_id)] = 'required';
-            }
-            if ($field->validation) {
-                $validation[strtolower($field->html_id)] = $field->validation;
-            }
-            $customAttributes[strtolower($field->html_id)] = $field->label[current_lang()] ?? $field->label[main_lang()] ?? '';
-        }
+        // $fastOrderFields = setting('reviews.fields', []);
+        // $customAttributes = [];
+        // foreach ($fastOrderFields as $field) {
+        //     $field = Field::query()->find($field['field_id']);
+        //     if ($field->required) {
+        //         $validation[strtolower($field->html_id)] = 'required';
+        //     }
+        //     if ($field->validation) {
+        //         $validation[strtolower($field->html_id)] = $field->validation;
+        //     }
+        //     $customAttributes[strtolower($field->html_id)] = $field->label[current_lang()] ?? $field->label[main_lang()] ?? '';
+        // }
         $validator = Validator::make($request->all(), $validation);
-        $validator->setAttributeNames($customAttributes);
+        // $validator->setAttributeNames($customAttributes);
         if ($validator->fails()) {
             return new ScmsResponse(false, [], $validator->errors()->toArray());
         }
-        $data = [];
-        foreach ($request->except(['_token', 'product_id', 'rating']) as $key => $value) {
-            $field = Field::query()->where('html_id', $key)->first();
-            if ($field) {
-                $data[$field->name] = $value;
-            }
-        }
+        // $data = [];
+        // foreach ($request->except(['_token', 'product_id', 'rating']) as $key => $value) {
+        //     $field = Field::query()->where('html_id', $key)->first();
+        //     if ($field) {
+        //         $data[$field->name] = $value;
+        //     }
+        // }
+        $data = $request->except(['_token', 'product_id', 'rating']);
         ProductReview::create([
             'product_id' => $request->product_id,
             'rating' => $request->rating,
