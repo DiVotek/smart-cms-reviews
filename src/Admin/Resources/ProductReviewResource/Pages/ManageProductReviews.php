b6a8b9e6b0856c13e8ce55ede39cafc3a53e189d
MaxBoiko21 <92517114+MaxBoiko21@users.noreply.github.com>
2025-03-31 08:58:25 +0000

    Fix styling

--- src/Admin/Resources/ProductReviewResource/Pages/ManageProductReviews.php
+++ src/Admin/Resources/ProductReviewResource/Pages/ManageProductReviews.php
@@ -6,12 +6,9 @@ use Filament\Actions;
 use Filament\Actions\Action as ActionsAction;
 use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\Hidden;
-use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Pages\ManageRecords;
-use SmartCms\Core\Models\Field;
 use SmartCms\Reviews\Admin\Resources\ProductReviewResource;
 
 class ManageProductReviews extends ManageRecords
@@ -22,8 +19,9 @@ class ManageProductReviews extends ManageRecords
     {
         $user_notification_fields = [];
         foreach (get_active_languages() as $lang) {
-            $user_notification_fields[] = Hidden::make('reviews.user_notification.' . $lang->slug);
+            $user_notification_fields[] = Hidden::make('reviews.user_notification.'.$lang->slug);
         }
+
         return [
             Actions\Action::make('help')
                 ->help('Reviews help'),
@@ -46,12 +44,13 @@ class ManageProductReviews extends ManageRecords
                                 if (count($translates) == count(get_active_languages()) + 1) {
                                     return 'heroicon-o-check-circle';
                                 }
+
                                 return 'heroicon-o-exclamation-circle';
                             })->form(function ($form) {
                                 $fields = [];
                                 $languages = get_active_languages();
                                 foreach ($languages as $language) {
-                                    $fields[] = TextInput::make($language->slug)->label(__('reviews::trans.user_notification') . ' (' . $language->name . ')');
+                                    $fields[] = TextInput::make($language->slug)->label(__('reviews::trans.user_notification').' ('.$language->name.')');
                                 }
 
                                 return $form->schema($fields);
@@ -61,7 +60,7 @@ class ManageProductReviews extends ManageRecords
                             })
                             ->action(function ($data, $set) {
                                 foreach ($data as $key => $value) {
-                                    $set('reviews.user_notification.' . $key, $value);
+                                    $set('reviews.user_notification.'.$key, $value);
                                 }
                             })),
                 ])
